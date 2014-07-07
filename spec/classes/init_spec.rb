678e37750b3f063b213cdf422621b6a80f0dac39
Rafa Avila <rafa.avim@gmail.com>
2014-07-07 15:43:24 -0500

    renaming to do not use Uppercase chars only lowercase, the default spec is passing, adding to dummy files to commit folder structure.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
-describe 'SemanticLogging' do
+describe 'semanticlogging', :type => 'class' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('SemanticLogging') }
+    it { should contain_class('semanticlogging') }
   end
 end

dfd02f36d942a7b1bf60a8f4f13404b3ff178642
Rafa Avila <rafa.avim@gmail.com>
2014-07-08 12:46:15 -0500

    add first spec of the project, aptgetupdate class

--- manifests/aptgetupdate.pp
+++ manifests/aptgetupdate.pp
@@ -3,4 +3,14 @@
 # Author: Name Surname <rafa.avim@gmail.com>
 class semanticlogging::aptgetupdate {
     # puppet code
+
+    Exec {
+    	path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ]
+    }
+
+    exec { "aptgetupdate":
+  		user      => root,
+  		group     => root,
+		command   => "apt-get update"
+	}
 }
--- spec/classes/semanticlogging_aptgetupdate_spec.rb
+++ spec/classes/semanticlogging_aptgetupdate_spec.rb
@@ -3,5 +3,7 @@ require 'spec_helper'
 describe 'semanticlogging::aptgetupdate', :type => 'class' do
 	context 'with defaults for all parameters' do
 		it { should contain_class('semanticlogging::aptgetupdate') }
+		it { should have_exec_resource_count(1) }
+		it { should contain_exec('aptgetupdate').with_command('apt-get update') }
 	end
 end
